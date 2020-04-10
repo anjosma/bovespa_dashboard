bad60d3653cb31c9cbf1499ed88b6c8bd83875bb
Matheus Felipe dos Anjos <mfa@smartbank.com.br>
2020-04-09 23:48:31 -0300

    Added option to change chaert and new metrics (return, price difference) added.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 import plotly.graph_objects as go
 
 from bovespa_dashboard.connection.database import Postgres
-from bovespa_dashboard.common.utils import load_file_json
+from bovespa_dashboard.common.utils import load_file_json, database_result_to_named_tuple
 
 configs_db = load_file_json("./configuration/database.json")
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -18,26 +18,26 @@ psql = Postgres(
         user=configs_db.get("USER"),
         password=configs_db.get("PASSWORD"),
         port=configs_db.get("PORT"),
-        database=configs_db.get("DATABASE"))
+        database=configs_db.get("DATABASE")
+		)
 
 cursor = psql.get_cursor
-cursor.execute("select tablename from pg_tables where schemaname='stocks'")
+cursor.execute(
+	"""
+	SELECT tablename
+	FROM pg_tables
+	WHERE schemaname='stocks'
+	"""
+)
 
 stocks = [s[0].upper() for s in cursor.fetchall()]
 stocks.sort()
 
-values_type = [("Abertura", "open"), ("Fechamento", "close"), ("Mínima", "low"), ("Alta", "high"), ("Volume", "volume")]
-
-nav_menu = html.Div([
-    html.Ul([
-            html.Li([
-                    dcc.Link('Page A', href='/page-a')
-                    ], className=''),
-            html.Li([
-                    dcc.Link('Page B', href='/page-b')
-                    ], className=''),
-            ], className='nav navbar-nav')
-], className='navbar navbar-default navbar-static-top')
+values_type_simple = [("Abertura", "open"), ("Fechamento", "close"),
+	("Mínima", "low"),("Alta", "high"), ("Volume", "volume")]
+values_type_computed = [("Diferença", "price_diff"), ("Retorno", "daily_return")]
+values_all = values_type_simple + values_type_computed
+values_all.sort()
 
 app.layout = html.Div([
 	
@@ -46,7 +46,7 @@ app.layout = html.Div([
 			html.Label('Período'),
 			dcc.DatePickerRange(
 				id='date-picker-range-historical',
-				start_date=datetime.date(datetime.now()-timedelta(days=30*6)),
+				start_date=datetime.date(datetime.now()-timedelta(days=30)),
 				end_date=datetime.date(datetime.now()),
 				display_format="D/M/Y"
 			),
@@ -63,13 +63,13 @@ app.layout = html.Div([
 		], style={'width': '48%', 'display': 'inline-block'}),
 	
 		html.Div([
-			html.Label('Preço'),
+			html.Label('Valor'),
 			dcc.Dropdown(
 				id='stocks-price-type-historical',
 				options=[
-					{'label': v[0], 'value': v[1]} for v in values_type
+					{'label': v[0], 'value': v[1]} for v in values_all
 				],
-				value=values_type[1][1]
+				value=values_type_simple[1][1]
 			),
 		], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
 	]),
@@ -94,10 +94,21 @@ app.layout = html.Div([
 				],
 				multi=False,
 				value="PETR3"
-			)
+			),
 		], style={'width': '48%', 'display': 'inline-block'}),
-	]),
 
+		html.Div([
+			html.Label('Gráfico'),
+			dcc.Dropdown(
+				id='stocks-selection-ohlc',
+				options=[
+					{'label': s, 'value': s} for s in ["Candlestick", "Ohlc"]
+				],
+				multi=False,
+				value="Candlestick"
+			)
+		], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+	]),
 	dcc.Graph(id='stocks-candle'),
 ])
 
@@ -108,22 +119,60 @@ app.layout = html.Div([
 	Input('date-picker-range-historical', 'end_date'),
 	Input('stocks-price-type-historical', 'value')])
 def updade_stocks_historical(stocks: list, start_date: str, end_date: str, value_type: str):
-	value_type_label = [l[0] for l in values_type if l[1] == value_type][0]
+	value_type_label = [l[0] for l in values_all if l[1] == value_type][0]
 	fig = go.Figure()
 	for stock in stocks:
-		query = "SELECT date, {value_type} FROM stocks.{stock} \
-			WHERE date >= '{start_date}' AND date <= '{end_date}' AND close notnull"
+		if value_type in [l[1] for l in values_type_simple]:
+			query = \
+				"""
+				SELECT date, {value_type}
+				FROM stocks.{stock}
+				WHERE date >= '{start_date}'
+					AND date <= '{end_date}'
+					AND CLOSE NOTNULL
+				"""
+		else:
+			query = \
+				"""
+				SELECT date, {value_type}
+				FROM (
+					SELECT *,
+						(price_diff / close) AS daily_return
+					FROM
+					(SELECT *,
+							(previous_close - close) AS price_diff
+					FROM
+						(SELECT *,
+								LAG(close, 1) over() AS previous_close
+						FROM
+							(SELECT date, close
+							FROM stocks.{stock}
+							ORDER BY date) A
+						WHERE 
+							CLOSE NOTNULL ) B) C) D
+				WHERE date >= '{start_date}'
+					AND date <= '{end_date}'
+				"""
+		
 		formatted = query.format(value_type=value_type, stock=stock.lower(), start_date=start_date, end_date=end_date)
 		
 		cursor.execute(formatted)
-		results_stock = cursor.fetchall()
-		
-		dates = [t[0] for t in results_stock]
-		values = [v[1] for v in results_stock]
+		result = database_result_to_named_tuple(cursor)
 
-		fig.add_trace(
-			go.Scatter(x=dates, y=values, mode="lines", name=stock, connectgaps=False)
-		)
+		if value_type in [l[1] for l in values_type_simple]:
+			fig.add_trace(
+				go.Scatter(
+					x=result.date, y=getattr(result, value_type), 
+					mode="lines", name=stock, connectgaps=False)
+			)
+		else:
+			fig.add_trace(
+				go.Waterfall(
+					name=stock,
+					x=result.date, y=getattr(result, value_type),
+
+				)
+			)
 	return fig.update_layout(
 		title=dict(
 			text="Histórico de Valor ({label})".format(
@@ -132,7 +181,8 @@ def updade_stocks_historical(stocks: list, start_date: str, end_date: str, value
 		),
 		plot_bgcolor="white",
 		xaxis=dict(
-			title="Data"
+			title="Data",
+			type="category"
 		),
 		yaxis=dict(
 			title="Valor"
@@ -143,26 +193,33 @@ def updade_stocks_historical(stocks: list, start_date: str, end_date: str, value
     Output('stocks-candle', 'figure'),
     [Input('stocks-selection-candle', 'value'), 
 	Input('date-picker-range-candle', 'start_date'),
-	Input('date-picker-range-candle', 'end_date')])
-def updade_stocks_candle(stock: str, start_date: str, end_date: str):
-	query = "SELECT date, low, open, close, high FROM stocks.{stock} \
-			WHERE date >= '{start_date}' AND date <= '{end_date}' AND close notnull"
+	Input('date-picker-range-candle', 'end_date'),
+	Input('stocks-selection-ohlc', "value")])
+def updade_stocks_candle(stock: str, start_date: str, end_date: str, chart_type: str):
+	query = \
+		"""
+		SELECT date, low,
+            	open,
+            	close,
+            	high
+		FROM stocks.{stock}
+		WHERE date >= '{start_date}'
+			AND date <= '{end_date}'
+			AND CLOSE NOTNULL
+		"""
 	formatted = query.format(stock=stock.lower(), start_date=start_date, end_date=end_date)
 		
 	cursor.execute(formatted)
-	results_stock = cursor.fetchall()
-		
-	dates = [t[0] for t in results_stock]
-	low = [v[1] for v in results_stock]
-	open = [v[2] for v in results_stock]
-	close = [v[3] for v in results_stock]
-	high = [v[4] for v in results_stock]
+	result = database_result_to_named_tuple(cursor)
 
-	return go.Figure(
-		go.Candlestick(
-			x=dates, low=low, open=open, close=close, high=high
+	fig = go.Figure(
+		getattr(go, chart_type)(
+			x=result.date, low=result.low, 
+			open=result.open, close=result.close, high=result.high
 		)
-	).update_layout(
+	)
+
+	return fig.update_layout(
 		xaxis_rangeslider_visible=False,
 		title=dict(
 			text="Histórico de Variação ({stock})".format(stock=stock),
@@ -170,7 +227,8 @@ def updade_stocks_candle(stock: str, start_date: str, end_date: str):
 		),
 		plot_bgcolor="white",
 		xaxis=dict(
-			title="Data"
+			title="Data",
+			type="category"
 		),
 		yaxis=dict(
 			title="Variação"
--- bovespa_dashboard/common/utils.py
+++ bovespa_dashboard/common/utils.py
@@ -1,6 +1,7 @@
 import json
 import os
 from datetime import datetime
+import collections
 
 def load_file_json(file_name: str) -> dict:
     with open(file_name, 'r') as json_file:
@@ -10,3 +11,12 @@ def create_dir(file_path: str) -> None:
     path = os.path.dirname(file_path)
     if not os.path.exists(path):
         os.makedirs(path)
+
+def database_result_to_named_tuple(cursor):
+    columns = [desc[0] for desc in cursor.description]
+    results_stock = cursor.fetchall()
+    stock_values={}
+    for i, column in enumerate(columns):
+        stock_values[column] = [t[i] for t in results_stock]
+    named_tuple = collections.namedtuple("results", sorted(stock_values))
+    return named_tuple(**stock_values)
